5062e4d98beba0bc647d0986289d493c1df3fbe1
Laveen <laveen2512@gmail.com>
2020-11-02 23:33:28 +0000

    Added preamble to exercise 1

--- Year 1/CS118/Coursework 1/Ex1.java
+++ Year 1/CS118/Coursework 1/Ex1.java
@@ -4,9 +4,22 @@
 * Author: Stephen Jarvis
 */
 
+/**
+ * <h1> Preamble for exercise 1 </h1>
+ * To ensure that a robot does not hit a wall, I used the part of the code from DumboController.java 
+ * which converts a random number into a direction and placed this into a loop. 
+ * This was done so that I was able to reuse that block of code repeatedly until the randomly chosen direction 
+ * happened to not be facing at a wall. I checked that the direction was not facing a wall 
+ * using {@code robot.look(direction)} and comparing this to {@code IRobot.WALL} .
+ * I also created a string variable called logEntry, as this would allow me to add the appropriate words and phrases as the program 
+ * determined what type of road and direction the robot was going in. This could then be printed out at the end just before the robot moves.
+ * This approach also makes the code easier to maintain and , since if the customer requires any further additions to the code then 
+ * only minor changes are needed.
+ */
+
 import uk.ac.warwick.dcs.maze.logic.IRobot;
 
-public class DumboController {
+public class Ex1 {
 
 	public void controlRobot(IRobot robot) {
 
