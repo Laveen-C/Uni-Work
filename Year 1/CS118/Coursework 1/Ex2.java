ee50e99cbb81e91f26e9ec4e92aeca673cc0d11e
Laveen <laveen2512@gmail.com>
2020-11-03 01:08:18 +0000

    Added comments to the switch statement

--- Year 1/CS118/Coursework 1/Ex2.java
+++ Year 1/CS118/Coursework 1/Ex2.java
@@ -4,20 +4,6 @@
  * Author: Stephen Jarvis
  */
 
-/*
- * Preamble for exercise 1:
- *
- * To ensure that a robot does not hit a wall, I used the part of the code from DumboController.java 
- * which converts a random number into a direction and placed this into a loop. 
- * This was done so that I was able to reuse that block of code repeatedly until the randomly chosen direction 
- * happened to not be facing at a wall. I checked that the direction was not facing a wall 
- * using "robot.look(direction)" and comparing this to "IRobot.WALL" .
- * I also created a string variable called logEntry, as this would allow me to add the appropriate words and phrases as the program 
- * determined what type of path and direction the robot was going in. This could then be printed out at the end just before the robot moves.
- * This approach also makes the code easier to maintain and , since if the customer requires any further additions to the code then 
- * only minor changes are needed.
- */
-
 import uk.ac.warwick.dcs.maze.logic.IRobot;
 
 public class Ex1 {
@@ -74,7 +60,7 @@ public class Ex1 {
 
 		}
 
-		switch (wallCount) { // Determining the type of path the robot is on, based on the number of adjacent walls
+		switch (wallCount) {
 		
 			case 0:
 				phraseToAdd = " at a crossroads";
@@ -91,7 +77,7 @@ public class Ex1 {
 				
 		}
 
-		logEntry += phraseToAdd; // Adding the type of path the robot is on
+		logEntry += phraseToAdd; // Adding the type of road the robot is on
 
 		System.out.println(logEntry);
 
