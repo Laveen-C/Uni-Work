5eeb65eea42f93151eca4c4af4f51ea4f3257906
Laveen <laveen2512@gmail.com>
2020-11-03 00:02:13 +0000

    Updated preamble for exercise 1

--- Year 1/CS118/Coursework 1/Ex1.java
+++ Year 1/CS118/Coursework 1/Ex1.java
@@ -1,16 +1,17 @@
 /*
-* File: DumboController.java
-* Created: 17 September 2002, 00:34
-* Author: Stephen Jarvis
-*/
+ * File: DumboController.java
+ * Created: 17 September 2002, 00:34
+ * Author: Stephen Jarvis
+ */
 
-/**
- * <h1> Preamble for exercise 1 </h1>
+/*
+ * Preamble for exercise 1:
+ *
  * To ensure that a robot does not hit a wall, I used the part of the code from DumboController.java 
  * which converts a random number into a direction and placed this into a loop. 
  * This was done so that I was able to reuse that block of code repeatedly until the randomly chosen direction 
  * happened to not be facing at a wall. I checked that the direction was not facing a wall 
- * using {@code robot.look(direction)} and comparing this to {@code IRobot.WALL} .
+ * using "robot.look(direction)" and comparing this to "IRobot.WALL" .
  * I also created a string variable called logEntry, as this would allow me to add the appropriate words and phrases as the program 
  * determined what type of road and direction the robot was going in. This could then be printed out at the end just before the robot moves.
  * This approach also makes the code easier to maintain and , since if the customer requires any further additions to the code then 
@@ -31,7 +32,7 @@ public class Ex1 {
 
 		// Removing collisions
 		while (true) {	// Repeatedly generate directions until we choose one that is not into a wall
-
+		
 			// Select a random number
 			randno = (int) Math.round(Math.random()*3);
 
@@ -65,14 +66,16 @@ public class Ex1 {
 		logEntry += phraseToAdd; // Adding the direction on to logEntry
 
 		for (int i = 0; i < 4; i++) { // Iterating to get the number of walls neighbouring the robot's current square
-
+			
 			int square = IRobot.AHEAD + i;
 
 			if (robot.look(square) == IRobot.WALL)
-				wallCount += 1;			
+				wallCount += 1;	
+
 		}
 
 		switch (wallCount) {
+		
 			case 0:
 				phraseToAdd = " at a crossroads";
 				break;
@@ -85,6 +88,7 @@ public class Ex1 {
 			case 3:
 				phraseToAdd = " at a deadend";
 				break;
+				
 		}
 
 		logEntry += phraseToAdd; // Adding the type of road the robot is on
